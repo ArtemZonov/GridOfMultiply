90fba336c5f8785986ba99ebf6b0b21c52c00e3c
ARTEM <jkda@ukr.net>
2021-07-15 23:56:58 +0300

    first started

--- app/src/main/java/com/jkdajac/gridofmultiply/MainActivity.kt
+++ app/src/main/java/com/jkdajac/gridofmultiply/MainActivity.kt
@@ -2,10 +2,25 @@ package com.jkdajac.gridofmultiply
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btOne.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, FirstFragment.newInstance("ff", "dd"))
+                .commit()
+        }
+
+        btTwo.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, SecondFragment.newInstance("ff", "dd"))
+                .commit()
+        }
     }
 }
