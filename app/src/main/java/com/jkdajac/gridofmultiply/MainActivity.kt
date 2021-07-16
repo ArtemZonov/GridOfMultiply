04a77037ee6b630c12d42576dd9835565e6ea2a6
ARTEM <jkda@ukr.net>
2021-07-16 21:06:14 +0300

    completed fragments

--- app/src/main/java/com/jkdajac/gridofmultiply/MainActivity.kt
+++ app/src/main/java/com/jkdajac/gridofmultiply/MainActivity.kt
@@ -30,5 +30,56 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fragmentContainerView, ThirdFragment.newInstance("ff", "dd"))
                 .commit()
         }
+
+        btFour.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, FourthFragment.newInstance("ff", "dd"))
+                .commit()
+        }
+
+        btFive.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, FifthFragment.newInstance("ff", "dd"))
+                .commit()
+        }
+
+        btSix.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, SixthFragment.newInstance("ff", "dd"))
+                .commit()
+        }
+
+        btSeven.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, SeventhFragment.newInstance("ff", "dd"))
+                .commit()
+        }
+
+        btEight.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, EighthFragment.newInstance("ff", "dd"))
+                .commit()
+        }
+
+        btNine.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, NinethFragment.newInstance("ff", "dd"))
+                .commit()
+        }
+
+        btTen.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, TenthFragment.newInstance("ff", "dd"))
+                .commit()
+        }
+
+
     }
 }
